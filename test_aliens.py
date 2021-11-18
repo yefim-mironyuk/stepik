949c1b68a9c1eb125a1481486996a0b27e22c792
yefim-mironyuk <efimmironyk@gmail.com>
2021-11-18 16:26:29 +0300

    find message task

--- test_aliens.py
+++ test_aliens.py
@@ -15,14 +15,15 @@ def browser():
 
 @pytest.mark.parametrize('links', ['895', '896', '897', '898', '899', '903', '904', '905'])
 def test_message_finder(browser, links):
-    link = f'https://stepik.org/lesson/236{links}/step/1'
-    browser.get(link)
-    answer = str(math.log(int(time.time())))
-    browser.implicitly_wait(10)
-    input1 = browser.find_element_by_tag_name('textarea')
-    input1.send_keys(answer)
-    button = browser.find_element_by_class_name('submit-submission')
-    button.click()
-    browser.implicitly_wait(10)
-    output1 = browser.find_element_by_class_name("smart-hints__hint")
-    assert output1.text == 'Correct!', 'MESSAGE IS HERE:'
+        link = f'https://stepik.org/lesson/236{links}/step/1'
+        browser.get(link)
+        answer = str(math.log(int(time.time())))
+        browser.implicitly_wait(10)
+        input1 = browser.find_element_by_tag_name('textarea')
+        input1.send_keys(answer)
+        button = browser.find_element_by_class_name('submit-submission')
+        button.click()
+        browser.implicitly_wait(10)
+        output1 = browser.find_element_by_class_name("smart-hints__hint")
+        assert output1.text == 'Correct!', 'MESSAGE IS HERE:'
+
